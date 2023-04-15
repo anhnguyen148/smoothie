1bbc7ddee86cefe45b70b208450680f8ba3f7f46
Quan Nguyen <nguyenquan263@gmail.com>
2023-04-15 13:26:59 -0500

    update env and gitignore

--- helpers/DatabaseHelper.py
+++ helpers/DatabaseHelper.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("mysql://qnguyen:Minhquan263@nside.guru:3306/smoothie")
+load_dotenv()
+engine = create_engine(os.getenv("DB_URL"))
 dbSession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
