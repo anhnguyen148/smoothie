3018f0bb158cdda20b352a71994bab59dabbba25
Quan Nguyen <nguyenquan263@gmail.com>
2023-04-16 20:49:50 -0500

    current state

--- controllers/BranchController.py
+++ controllers/BranchController.py
@@ -27,3 +27,36 @@ async def addNewBranch(inputBranch: BranchDTO):
 
     return apiResponse
 
+@branchController.put("/branch/{branchId}")
+async def updateBranch(branchId: int, inputBranch: BranchDTO):
+    targetBranch = Branch.query.filter(Branch.branch_id == branchId).first()
+
+    if targetBranch is None:
+        apiResponse = APIResponseDTO().badResponse(400, "Target branch is not found.", None)
+    else:
+        targetBranch.name = inputBranch.name
+        targetBranch.location = inputBranch.location
+        targetBranch.phone = inputBranch.phone
+
+        dbSession.flush()
+        dbSession.commit()
+
+        apiResponse = APIResponseDTO().successResponse("Update the branch successfully.", None)
+
+    return apiResponse
+
+@branchController.delete("/drink/{branchId}")
+async def deleteBranch(branchId: int):
+    targetBranch = Branch.query.filter(Branch.branch_id == branchId).first()
+
+    if targetBranch is None:
+        apiResponse = APIResponseDTO().badResponse(400, "Target branch is not found.", None)
+    else:
+        dbSession.delete(targetBranch)
+
+        dbSession.flush()
+        dbSession.commit()
+
+        apiResponse = APIResponseDTO().successResponse("Delete the branch succesfully!", None)
+
+    return apiResponse
